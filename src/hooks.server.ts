a5654f09cf70afef4bc2191bef86d8b2afc9669f
can4red <CAN4red@yandex.ru>
2025-04-26 16:12:25 +0300

    refator: remove client initialization

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,9 +6,9 @@ import { paraglideMiddleware } from "$lib/paraglide/server";
 import { WS_PORT } from "$env/static/private";
 import ServerApi from "$lib/server/api/ServerApi";
 
-// @ts-expect-error: ignore ts(2307)
-import { SERVER_URL } from "$env/static/private";
-import { ClientApi } from "$lib/client/ClienApi";
+// // @ts-expect-error: ignore ts(2307)
+// import { SERVER_URL } from "$env/static/private";
+// import { ClientApi } from "$lib/client/ClienApi";
 
 const handleParaglide: Handle = ({ event, resolve }) =>
   paraglideMiddleware(event.request, ({ request, locale }) => {
@@ -27,16 +27,14 @@ const initServer: ServerInit = () => {
   serverApi.start(portNumber);
 };
 
-const initClient = () => {
-  const serverUrl = SERVER_URL as string;
-  const clientApi = new ClientApi(serverUrl);
-  clientApi.enterDocument("lol");
-  clientApi.updateDocument("lol", "new content");
-  return clientApi;
-};
+// const initClient = () => {
+//   const serverUrl = SERVER_URL as string;
+//   const clientApi = new ClientApi(serverUrl);
+//   return clientApi;
+// };
 
 export const handle: Handle = ({ event, resolve }) => {
-  initClient();
+  // initClient();
   return handleParaglide({ event, resolve });
 };
 
