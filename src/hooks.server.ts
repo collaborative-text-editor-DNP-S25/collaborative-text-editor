64b48a6c6fc130608a6edcbc3b765fe0fa7882aa
can4red <CAN4red@yandex.ru>
2025-04-28 11:19:25 +0300

    .

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,9 +6,9 @@ import { paraglideMiddleware } from "$lib/paraglide/server";
 import { WS_PORT } from "$env/static/private";
 import ServerApi from "$lib/server/api/ServerApi";
 
-// @ts-expect-error: ignore ts(2307)
-import { SERVER_URL } from "$env/static/private";
-import { ClientApi } from "$lib/server/api/ClientApi";
+// // @ts-expect-error: ignore ts(2307)
+// import { SERVER_URL } from "$env/static/private";
+// import { ClientApi } from "$lib/server/api/ClientApi";
 
 const handleParaglide: Handle = ({ event, resolve }) =>
   paraglideMiddleware(event.request, ({ request, locale }) => {
@@ -27,21 +27,35 @@ const initServer: ServerInit = () => {
   serverApi.start(portNumber);
 };
 
-const initClient = () => {
-  const serverUrl = SERVER_URL as string;
-  const clientApi = new ClientApi(serverUrl);
+// const initClient = () => {
+//   const serverUrl = SERVER_URL as string;
+//   const clientApi = new ClientApi(serverUrl);
 
+<<<<<<< HEAD
   clientApi.createDocument();
   clientApi.enterDocument({ id: "doc-0" });
   clientApi.updateDocument({ id: "doc-0" }, "doc update");
+=======
+//   clientApi.onMessage((message) => {
+//     console.log(message);
+//   });
 
-  clientApi.getAllDocuments();
+//   clientApi.onGetAllDocuments((documentIds) => {
+//     console.log(documentIds);
+//   });
 
-  return clientApi;
-};
+//   clientApi.createDocument();
+//   clientApi.enterDocument({ id: "doc-0" });
+//   clientApi.updateDocument({ id: "doc-0" }, "doc update");
+>>>>>>> ea3423d (.)
+
+//   clientApi.getAllDocuments();
+
+//   return clientApi;
+// };
 
 export const handle: Handle = ({ event, resolve }) => {
-  initClient();
+  // initClient();
   return handleParaglide({ event, resolve });
 };
 
