0354505ce3bd722dc34e83b0d25a9105817545f5
Rashid Badamshin <rashidbadamshin@gmail.com>
2025-04-29 17:51:42 +0300

    fixed reviewed changes

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const config = {
   preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({}),
-    
-  }
+  kit: { adapter: adapter() },
 };
 
 export default config;
