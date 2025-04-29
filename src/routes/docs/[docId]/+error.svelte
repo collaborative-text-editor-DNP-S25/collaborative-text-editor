c28945773b4eed3e7054be75c733f1e9c0729e53
Asqar Arslanov <asqarslanov@gmail.com>
2025-04-29 16:01:41 +0300

    feat: improve 404 page

--- src/routes/docs/[docId]/+error.svelte
+++ src/routes/docs/[docId]/+error.svelte
@@ -2,4 +2,8 @@
   import BackButton from "$lib/components/BackButton.svelte";
 </script>
 
-<BackButton />
+<main class="flex h-full w-full flex-col items-center justify-center gap-4 p-4">
+  This document does’t exist.
+
+  <BackButton />
+</main>
